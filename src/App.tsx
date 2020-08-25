ad63ab26cac9c1750e9a0290acca1d307ed11596
Armanco <akolahan@gmail.com>
2020-08-25 22:15:04 +0200

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from "react-router";
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    </>
+      <Router>
+            <Switch>
+            </Switch>
+      </Router>
   );
 }
 
